53f66e44b21c413a240aeb1a1f0a5c9bf87ebcef
Christophe Nouguier <christophe.nouguier@free.fr>
2019-02-12 23:39:25 +0100

    build: try to fix android / ios

--- travis.ios.sh
+++ travis.ios.sh
@@ -12,37 +12,30 @@ else
 	echo -e "machine github.com\n  login $GITHUB_TOKEN" > ~/.netrc
 	git clone https://github.com/kalisio/kApp-secrets
 
-	# Create the keychain
-	KEY_CHAIN_NAME=ios-build.keychain
-	KEY_CHAIN_PASSWORD=travis
-
 	# Create a custom keychain
-	security create-keychain -p $KEY_CHAIN_PASSWORD $KEY_CHAIN_NAME
-	security default-keychain -s $KEY_CHAIN_NAME
-	security unlock-keychain -p $KEY_CHAIN_PASSWORD $KEY_CHAIN_NAME
-  
-	#security set-keychain-settings -t 3600 -u $KEY_CHAIN_NAME
-
-	#security list-keychains -s $KEY_CHAIN
+	security create-keychain -p travis ios-build.keychain
+	security default-keychain -s ios-build.keychain
+	security unlock-keychain -p travis ios-build.keychain
+  security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
 	# Add certificates to keychain and allow codesign to access them
 	# see: https://github.com/travis-ci/travis-ci/issues/6791#issuecomment-261215038
-	security import kApp-secrets/ios/AppleWWDRCA.cer -k $KEY_CHAIN_NAME -A
-	security import kApp-secrets/ios/ios_distribution.cer -k $KEY_CHAIN_NAME -A
-	security import kApp-secrets/ios/ios_distribution.p12 -k $KEY_CHAIN_NAME -P $APPLE_KEY_PASSWORD -A
+	security import kApp-secrets/ios/AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+	security import kApp-secrets/ios/ios_distribution.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+	security import kApp-secrets/ios/ios_distribution.p12 -k ~/Library/Keychains/ios-build.keychain -P $APPLE_KEY_PASSWORD -T /usr/bin/codesign
 
 	# see: https://docs.travis-ci.com/user/common-build-problems/#mac-macos-sierra-1012-code-signing-errors
-  security set-key-partition-list -S apple-tool:,apple: -s -k keychainPass $KEY_CHAIN_NAME
+  security set-key-partition-list -S apple-tool:,apple: -s -k keychainPass ios-build.keychain
 
 	# Install the required secret files requied to sign the app
 	cp kApp-secrets/ios/build.json cordova/.
 	mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-	cp kApp-secrets/ios/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/.
+	cp kApp-secrets/ios/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
 
 	# Build and deploy the app
 	npm run cordova:add:ios
 	npm run cordova:build:release
 
   # Backup the ios build to S3
-	aws s3 sync cordova/platforms/ios s3://kapp-builds/$TRAVIS_BUILD_NUMBER/ios
+	aws s3 sync cordova/platforms/ios s3://kapp-builds/$TRAVIS_BUILD_NUMBER/ios/build
 fi
