5a888d21ce087b92652712cfa7d96c4b64def162
Christophe Nouguier <christophe.nouguier@free.fr>
2019-02-12 15:26:21 +0100

    build: updated ios build following travis-ci recommendation

--- travis.ios.sh
+++ travis.ios.sh
@@ -13,24 +13,25 @@ else
 	git clone https://github.com/kalisio/kApp-secrets
 
 	# Create the keychain
-	KEY_CHAIN=ios-build.keychain
-	KEY_CHAIN_PASSWORD=travis
+	KEY_CHAIN_NAME=ios-build.keychain
+	KEY_CHAIN_PASSWORD=travis@kdk
 
 	# Create a custom keychain
-	security create-keychain -p $KEY_CHAIN_PASSWORD $KEY_CHAIN
-	security unlock-keychain -p $KEY_CHAIN_PASSWORD $KEY_CHAIN
-	security default-keychain -s $KEY_CHAIN
-  security set-keychain-settings -t 3600 -u $KEY_CHAIN
+	security create-keychain -p $KEY_CHAIN_PASSWORD $KEY_CHAIN_NAME
+	security default-keychain -s $KEY_CHAIN_NAME
+	security unlock-keychain -p $KEY_CHAIN_PASSWORD $KEY_CHAIN_NAME
+  
+	#security set-keychain-settings -t 3600 -u $KEY_CHAIN_NAME
 
-	security list-keychains -s $KEY_CHAIN
+	#security list-keychains -s $KEY_CHAIN
 
 	# Add certificates to keychain and allow codesign to access them
-	security import kApp-secrets/ios/AppleWWDRCA.cer -k $KEY_CHAIN -T /usr/bin/codesign
-	#security import kApp-secrets/ios/ios_development.cer -k $KEY_CHAIN -T /usr/bin/codesign
-	security import kApp-secrets/ios/ios_distribution.cer -k $KEY_CHAIN -T /usr/bin/codesign
-	security import kApp-secrets/ios/ios_distribution.p12 -k $KEY_CHAIN -P $APPLE_KEY_PASSWORD -T /usr/bin/codesign
+	security import kApp-secrets/ios/AppleWWDRCA.cer -k $KEY_CHAIN_NAME -T /usr/bin/codesign
+	security import kApp-secrets/ios/ios_distribution.cer -k $KEY_CHAIN_NAME -T /usr/bin/codesign
+	security import kApp-secrets/ios/ios_distribution.p12 -k $KEY_CHAIN_NAME -P $APPLE_KEY_PASSWORD -T /usr/bin/codesign
+
 	# see: https://docs.travis-ci.com/user/common-build-problems/#mac-macos-sierra-1012-code-signing-errors
-  security set-key-partition-list -S apple-tool:,apple: -s -k keychainPass $KEY_CHAIN
+  security set-key-partition-list -S apple-tool:,apple: -s -k keychainPass $KEY_CHAIN_NAME
 
 	# Install the required secret files requied to sign the app
 	cp kApp-secrets/ios/build.json cordova/.
