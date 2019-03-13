c62dec79b0bb4162cb4079cc3ea25204d09d24ff
Christophe Nouguier <christophe.nouguier@free.fr>
2019-03-13 16:32:18 +0100

    ci: updated ios/android scripts to handle the new workspace structure

--- .travis.android.sh
+++ .travis.android.sh
@@ -18,9 +18,8 @@ else
 	git clone -b $APP https://github.com/kalisio/kdk-workspaces workspace
 
 	# Install the required secret files requied to sign the app
-	ls workspace/$FLAVOR/android/
-	cp workspace/$FLAVOR/android/*.json cordova/
-	cp workspace/$FLAVOR/android/kalisio.keystore cordova/
+	cp workspace/common/android/*.json cordova/
+	cp workspace/common/android/kalisio.keystore cordova/
 	cp workspace/$FLAVOR/android/Appfile cordova/fastlane/
 	
 	travis_fold end "privision"
