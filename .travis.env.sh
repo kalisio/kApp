278073c228fc472f3bcaba103f42520af364ce31
Christophe Nouguier <christophe.nouguier@free.fr>
2019-03-15 00:22:34 +0100

    ci: wip #61

--- .travis.env.sh
+++ .travis.env.sh
@@ -3,19 +3,16 @@
 # Define required environement variobles according the flavor
 if [[ $TRAVIS_BRANCH == "master" ]]
 then
-	export DEBUG=
 	export FLAVOR=dev
 	export PACKAGE_ID=com.${AUTHOR:-kalisio}.$APP.dev
 fi
 if [[ $TRAVIS_BRANCH == "test" ]]
 then
-	export DEBUG=
 	export FLAVOR=test
 	export PACKAGE_ID=com.${AUTHOR:-kalisio}.$APP.test
 fi
 if [[ -n "$TRAVIS_TAG" ]]
 then
-	export DEBUG=
 	export FLAVOR=prod
 	export PACKAGE_ID=com.${AUTHOR:-kalisio}.$APP
 fi
@@ -27,14 +24,32 @@ export BUILDS_BUCKET=$APP-builds
 # Retrieve the environment variables stored in the workspace
 echo -e "machine github.com\n  login $GITHUB_TOKEN" > ~/.netrc
 git clone -b $APP https://github.com/kalisio/kdk-workspaces workspace
-cp workspace/$FLAVOR/.env .env
+cp workspace/common/.env .env
+if [ -e workspace/$FLAVOR/.env ]
+then
+	cat workspace/$FLAVOR/.env >> .env
+fi
 
 # Add computed variables
+echo "APP=$APP" >> .env
+echo "COMPOSE_PROJECT_NAME=$APP" >> .env
 echo "NODE_APP_INSTANCE=$FLAVOR" >> .env
+echo "VERSION=$VERSION" >> .env
+echo "VERSION_TAG=$VERSION-$FLAVOR" >> .env
 echo "BUILD_NUMBER=$TRAVIS_BUILD_NUMBER" >> .env
 
 set -a
 . .env
 set +a
 
+# Retrieve ci environement variables
+cp workspace/common/.travis.env .travis.env
+if [ -e workspace/$FLAVOR/.travis.env ]
+then
+	cat workspace/$FLAVOR/.travis.env >> .travis.env
+fi
+
+set -a
+. .travis.env
+set +a
 
