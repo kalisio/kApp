035e5accae238e8510709997ddb3e0edca274171
Luc Claustres <luc.claustres@orange.fr>
2019-07-24 19:03:44 +0200

    WIP https://github.com/kalisio/kdk/issues/31 (fixed terms in apps)

--- src/components/Terms.vue
+++ src/components/Terms.vue
@@ -1,21 +1,18 @@
 <template>
-  <q-card>
-    <q-card-section>
-      <k-markdown-viewer url="statics/kapp-terms.md"></k-markdown-viewer>
-    </q-card-section>
-  </q-card>
+  <div class="q-pa-md">
+    <q-card>
+      <q-card-section>
+        <k-markdown-viewer url="statics/kapp-terms.md"></k-markdown-viewer>
+      </q-card-section>
+    </q-card>
+  </div>
 </template>
 
 <script>
 import utils from '../utils'
-import { QCard, QCardSection } from 'quasar'
 
 export default {
   name: 'terms',
-  components: {
-    QCard,
-    QCardSection
-  },
   created () {
     this.$options.components['k-screen'] = utils.loadComponent('frame/KScreen')
     this.$options.components['k-markdown-viewer'] = utils.loadComponent('media/KMarkdownViewer')
