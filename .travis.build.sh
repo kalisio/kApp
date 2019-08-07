d214dba87d18b894aaf82b2cd83a3bce6cf4e706
Christophe Nouguier <christophe.nouguier@free.fr>
2019-08-07 12:04:57 +0200

    wip: https://github.com/kalisio/kdk/issues/31

--- .travis.build.sh
+++ .travis.build.sh
@@ -13,17 +13,16 @@ travis_fold start "build"
 # built artifact from the container to the host. Indeed the artifact is then copied to S3 
 # (see the deploy hook) and can be used by the following stages (i.e. Android and iOS).
 
+#
+# Build the docker image
+#
 if [[ $TRAVIS_COMMIT_MESSAGE != *"[skip build]"* ]]
 then
 	# Build the image
-	docker-compose -f deploy/app.yml -f deploy/app.build.yml build > build.log 2>&1
-  # Capture the build result
-	BUILD_CODE=$?
-	# Copy the log whatever the result
-	aws s3 cp build.log s3://$BUILDS_BUCKET/$BUILD_NUMBER/build.log
-	# Exit if an error has occured
-	if [ $BUILD_CODE -ne 0 ]; then
-		echo Build has failed with error: $BUILD_CODE
+	docker-compose -f deploy/app.yml -f deploy/app.build.yml build
+	ERROR_CODE=$?
+	if [ $ERROR_CODE -ne 0 ]; then
+		echo "Building the docker image has failed [error: $ERROR_CODE]"
 		exit 1
 	fi
 
@@ -31,7 +30,9 @@ then
 	docker tag kalisio/$APP kalisio/$APP:$VERSION_TAG
 	docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
 	docker push kalisio/$APP:$VERSION_TAG
-	if [ $? -eq 1 ]; then
+	ERROR_CODE=$?
+	if [ $ERROR_CODE -eq 1 ]; then
+	  echo "Pushing the docker image has failed [error: $ERROR_CODE]"
 		exit 1
 	fi
 fi
@@ -44,14 +45,16 @@ travis_fold end "build"
 travis_fold start "backup"
 
 # Copy the artifact from the container to the host
-# See https://docs.docker.com/compose/reference/envvars/#compose_project_name
-# explanation on the container name
+# See https://docs.docker.com/compose/reference/envvars/#compose_project_name to
+# get some explanation on the container name
 docker-compose -f deploy/app.yml up -d
 docker cp ${APP}_app_1:/opt/$APP/dist/spa dist
 
 # Backup the artifact to S3
 aws s3 sync dist s3://$BUILDS_BUCKET/$BUILD_NUMBER/www > /dev/null
-if [ $? -eq 1 ]; then
+ERROR_CODE=$?
+if [ $ERROR_CODE -eq 1 ]; then
+	echo "Copying the artifacr to S3 has failed [error: $ERROR_CODE]"
 	exit 1
 fi
 
