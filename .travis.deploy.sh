0d92c0c4f0e99139decff150b5c5f621a5437c97
Christophe Nouguier <christophe.nouguier@free.fr>
2019-10-17 07:23:40 +0200

    ci: enhanced deploy stage to use bastion

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -14,28 +14,28 @@ else
 	cp workspace/$FLAVOR/ssh.config ~/.ssh/config
 
   # Create app directory if needed 
-	ssh remote mkdir -p $APP
+	ssh -o StrictHostKeyChecking=no REMOTE_SERVER mkdir -p $APP
 
 	# Deploy environment file
-	scp .env remote:~/$APP/.env
+	scp .env REMOTE_SERVER:~/$APP/.env
 
 	# Deploy compose files
-	scp deploy/app.yml remote:~/$APP/app.yml
-	scp deploy/app.swarm.yml remote:~/$APP/app.swarm.yml
-	scp deploy/mongodb.yml remote:~/$APP/mongodb.yml
-	scp deploy/mongodb.swarm.yml remote:~/$APP/mongodb.swarm.yml
+	scp deploy/app.yml REMOTE_SERVER:~/$APP/app.yml
+	scp deploy/app.swarm.yml REMOTE_SERVER:~/$APP/app.swarm.yml
+	scp deploy/mongodb.yml REMOTE_SERVER:~/$APP/mongodb.yml
+	scp deploy/mongodb.swarm.yml REMOTE_SERVER:~/$APP/mongodb.swarm.yml
 
 	# Deploy utilities
-	scp deploy/deploy-app.sh remote:~/$APP
-	scp deploy/remove-app.sh remote:~/$APP
+	scp deploy/deploy-app.sh REMOTE_SERVER:~/$APP
+	scp deploy/remove-app.sh REMOTE_SERVER:~/$APP
 
 	# Deploy the stack
 	SUDO=""
 	if [ "$SSH_USER" != "root" ]; then
 		SUDO="sudo"
 	fi
-	ssh remote "cd $APP; chmod u+x ./remove-app.sh; chmod u+x ./deploy-app.sh"
-	ssh remote "cd $APP; ./remove-app.sh; k-swarm-prune; ./deploy-app.sh"
+	ssh REMOTE_SERVER "cd $APP; chmod u+x ./remove-app.sh; chmod u+x ./deploy-app.sh"
+	ssh REMOTE_SERVER "cd $APP; ./remove-app.sh; k-swarm-prune; ./deploy-app.sh"
 
 	travis_fold end "deploy"
 fi
