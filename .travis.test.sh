b0c08d74e7a3aa320149564e1f1f3603d0af06f6
Christophe Nouguier <christophe.nouguier@free.fr>
2019-12-09 21:18:43 +0100

    wip: Deployment should rely on an existing MongoDB instance #72

--- .travis.test.sh
+++ .travis.test.sh
@@ -4,9 +4,6 @@ then
 	echo "Skipping test stage"
 else
 	source .travis.env.sh
-  #DOCKER_HOST=172.17.0.1
-	DOCKER_HOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')
-	echo $DOCKER_HOST
 
 	# It first need to create the required network 
 	docker network create --attachable $DOCKER_NETWORK
@@ -20,8 +17,8 @@ else
 	mkdir server-coverage
 	chmod -R 777 server-coverage
 
-	# Run the API tests
-	docker-compose -f deploy/app.yml -f deploy/app.test.server.yml up app
+	# Run the tests
+	docker-compose -f deploy/app.yml -f deploy/mongodb.yml -f deploy/app.test.server.yml up app
 	ERROR_CODE=$?
 	if [ $ERROR_CODE -eq 1 ]; then
 		echo "Testing ${APP} API failed [error: $ERROR_CODE]"
@@ -47,7 +44,7 @@ else
 	chmod -R 777 client-screenshots
  
   # Run the app
-	docker-compose -f deploy/app.yml -f deploy/app.test.client.yml up -d app
+	docker-compose -f deploy/app.yml -f deploy/mongodb.yml -f deploy/app.test.client.yml up -d app
 	ERROR_CODE=$?
 	if [ $ERROR_CODE -eq 1 ]; then
 		echo "Running ${App} failed [error: $ERROR_CODE]"
@@ -55,7 +52,7 @@ else
 	fi
 
 	# Run client tests
-	docker-compose -f deploy/app.yml -f deploy/app.test.client.yml up testcafe
+	docker-compose -f deploy/app.yml -f deploy/mongodb.yml -f deploy/app.test.client.yml up testcafe
 	ERROR_CODE=$?
 	# Copy the screenshots whatever the result
 	aws s3 sync client-screenshots s3://$BUILDS_BUCKET/$BUILD_NUMBER/client-screenshots > /dev/null
