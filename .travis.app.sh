b40612013d3dec6d61ad3d421d7e0231fc2c11fa
Christophe Nouguier <christophe.nouguier@free.fr>
2020-01-29 10:43:29 +0100

    wip: Allow to run e2e on the targeted deployment platform (https://github.com/kalisio/kdk/issues/62) [skip-android skip-ios]

--- .travis.app.sh
+++ .travis.app.sh
@@ -36,10 +36,10 @@ tar -zcf kdk.tgz kdk
 docker build --build-arg APP=$APP --build-arg FLAVOR=$FLAVOR --build-arg BUILD_NUMBER=$BUILD_NUMBER -f dockerfile.app -t kalisio/$APP:$TAG . 
 check_code $? "Building the app docker image"
 
-docker build --build-arg APP=$APP --build-arg TAG=$TAG -f dockerfile.tests.api -t kalisio/$APP:$TAG-tests-api . 
+docker build --build-arg APP=$APP --build-arg TAG=$TAG -f dockerfile.tests.api -t kalisio/$APP:${TAG}_tests-api . 
 check_code $? "Building the tests api docker image"
 
-docker build --build-arg APP=$APP -f dockerfile.tests.client -t kalisio/$APP:$TAG-tests-client . 
+docker build --build-arg APP=$APP -f dockerfile.tests.client -t kalisio/$APP:${TAG}_tests-client . 
 check_code $? "Building the tests client docker image"
 
 travis_fold end "build"
@@ -56,10 +56,10 @@ check_code $? "Connecting to Docker"
 push_docker $APP $TAG $FLAVOR 
 
 # Push the tests api image to the hub
-push_docker $APP $TAG-tests-api- $FLAVOR-tests-api
+push_docker $APP ${TAG}_tests-api ${FLAVOR}_tests-api
 
 # Push the tests client image to the hub
-push_docker $APP $TAG-tests-client $FLAVOR-tests-client
+push_docker $APP ${TAG}_tests-client ${FLAVO}_tests-client
 
 # Copy the required keys and update the mode
 cp workspace/$FLAVOR/*.pem ~/.ssh/.
