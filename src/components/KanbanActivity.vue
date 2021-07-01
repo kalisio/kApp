af5a8e976b11209a4e94cee17444e2db9a635929
cnouguier <christophe.nouguier@free.fr>
2021-07-01 16:08:23 +0200

    wip: Refactor the layout using new KDK capabilities #100

--- src/components/KanbanActivity.vue
+++ src/components/KanbanActivity.vue
@@ -1,15 +1,14 @@
 <template>
-  <k-page padding>
+  <k-page padding @content-resized="onPageContentResized">
     <template v-slot:page-content>
       <!--
         Item list rendering
       -->
       <k-board 
-        service="documents" 
-        :categories="categories" 
-        :categoryField="categoryField"
-        :renderer="cardRenderer" 
-        :filter-query="filter.query" />
+        v-if="height"
+        class="q-pa-md"
+        :columns="boardColumns" 
+        :height="height" />
     </template>
     <!--
       Enable modal
@@ -24,6 +23,44 @@ import { mixins as kCoreMixins } from '@kalisio/kdk/core.client'
 export default {
   name: 'kanban-activity',
   mixins: [kCoreMixins.baseActivity()],
+  computed: {
+    columnWidth () {
+      if (this.$q.screen.lt.md) return 280
+      if (this.$q.screen.lt.lg) return 360
+      return 440
+    },
+    boardColumns () {
+      return [{
+        label: 'Sain',
+        value: 'todo',
+        props: {
+          service: 'documents',
+          renderer: { component: 'collection/KCard' },
+          baseQuery: Object.assign({ 'etat_sani': 'sain' })
+        },
+        width: this.columnWidth
+      }, {
+        label: 'Malade',
+        value: 'doing',
+        props: {
+          service: 'documents',
+          renderer: { component: 'collection/KCard' },
+          contextId: this.contextId,
+          baseQuery: Object.assign({ 'etat_sani': 'malade' })
+        },
+        width: this.columnWidth
+      }, {
+        label: 'Declin',
+        value: 'done',
+        props: {
+          service: 'documents',
+          renderer: { component: 'collection/KCard' },
+          baseQuery: Object.assign({ 'etat_sani': 'declin' })
+        },
+        width: this.columnWidth
+      }]
+    }
+  },
   data () {
     return {
       categories: null,
@@ -35,7 +72,8 @@ export default {
           { id: 'view-document', icon: 'las la-glasses', tooltip: 'KanbanActivity.VIEW_DOCUMENT',
             handler: (context) => this.$router.push({ name: 'view-document', params: { page: 'list', objectId: context.item._id } }) }
         ]
-      }
+      },
+      height: undefined
     }
   },
   methods: {
@@ -43,6 +81,9 @@ export default {
       kCoreMixins.baseActivity(this.activityName).methods.configureActivity.call(this)
       this.categories = this.activityOptions.categories
       this.categoryField = this.activityOptions.categoryField
+    },
+    onPageContentResized (size) {
+      this.height = size.height - 120
     }
   },
   beforeCreate () {
