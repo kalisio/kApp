9541e47131855a93f23f1c380c9bb9f18af483b2
Luc Claustres <luc.claustres@orange.fr>
2022-05-20 17:19:12 +0200

    wip: Upgrade to Quasar 2 / Vuejs 3 (#127)

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -1,15 +1,5 @@
-import { createI18n } from 'vue-i18n'
-import { utils as kdkCoreUtils } from '@kalisio/kdk/core.client'
+import { i18n } from '@kalisio/kdk/core.client'
 
 export default async ({ app }) => {
-  // Define the locale to be used
-  const fallbackLocale = kdkCoreUtils.getAppFallbackLocale()
-  const locale = kdkCoreUtils.getAppLocale()
-  // Create i18n instance using the translation bundles
-  app.use(createI18n({
-    locale,
-    fallbackLocale,
-    messages: await kdkCoreUtils.loadTranslations(['core', 'app'], locale, fallbackLocale),
-    silentFallbackWarn: true
-  }))
+  await i18n.initialize(app, ['core', 'app'])
 }
