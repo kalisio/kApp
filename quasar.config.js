ea70b53021e0eafe3131a06b7dec2c84b9fd6db3
cnouguier <christophe.nouguier@free.fr>
2022-08-31 13:32:36 +0200

    chore: added missing deps to use password-generator in browser

--- quasar.config.js
+++ quasar.config.js
@@ -89,6 +89,8 @@ module.exports = configure(function (ctx) {
         cfg.resolve.fallback = {
           fs: false,
           path: require.resolve('path-browserify'),
+          crypto: require.resolve('crypto-browserify'),
+          stream: require.resolve('stream-browserify'),
           url: require.resolve('url')
         },
         cfg.resolve.modules = [
