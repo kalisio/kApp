d0060e0566291390c759ff5afc785756bc72ec2d
Luc Claustres <luc.claustres@orange.fr>
2023-02-16 18:14:06 +0100

    chore: API client should now be initialized by applications

--- src/boot/kdk.js
+++ src/boot/kdk.js
@@ -4,9 +4,11 @@ import config from 'config'
 import { Notify } from 'quasar'
 import appHooks from '../main.hooks'
 import services from '../services'
-import { api, i18n, utils as kdkCoreUtils, Store, Layout, Events, beforeGuard, authenticationGuard } from '@kalisio/kdk/core.client'
+import { initializeApi, i18n, utils as kdkCoreUtils, Store, Layout, Events, beforeGuard, authenticationGuard } from '@kalisio/kdk/core.client'
 
 export default async ({ app }) => {
+  // Initiate the client
+  const api = initializeApi()
   // Setup app hooks
   api.hooks(appHooks)
   // Then all services
