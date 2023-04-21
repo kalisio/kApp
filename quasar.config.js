ac10e769bb5e477030c3ba659e9470c7e2e9f4bc
Tristan Greffe <tristan.greffe@hotmail.com>
2023-04-21 14:53:52 +0200

    wip: add configuration for automatic pwa reload & update #144

--- quasar.config.js
+++ quasar.config.js
@@ -259,8 +259,11 @@ module.exports = configure(function (ctx) {
 
     // https://v2.quasar.dev/quasar-cli-webpack/developing-pwa/configuring-pwa
     pwa: {
-      workboxPluginMode: 'InjectManifest', // 'GenerateSW' or 'InjectManifest'
-      workboxOptions: {}, // only for GenerateSW
+      workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
+      workboxOptions: {
+        skipWaiting: true,
+        clientsClaim: true
+      }, // only for GenerateSW
 
       // for the custom service worker ONLY (/src-pwa/custom-service-worker.[js|ts])
       // if using workbox in InjectManifest mode
