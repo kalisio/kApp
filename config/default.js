df19176d8d938c4dcf471d3662812d1e3f84c9e2
Tristan Greffe <tristan.greffe@hotmail.com>
2023-05-11 17:37:30 +0200

    wip: add PWA property to default configuration #144

--- config/default.js
+++ config/default.js
@@ -118,6 +118,7 @@ module.exports = {
   apiTimeout: 20000,
   transport: 'websocket', // Could be 'http' or 'websocket',
   appName: 'kApp',
+  pwa: process.env.PWA_ENV,
   // appLogo: 'kapp-logo.png',
   appWebsite: 'https://github.com/kalisio/kApp',
   publisher: 'Kalisio',
