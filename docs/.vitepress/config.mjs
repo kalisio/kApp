ef568dade9d40b37507ec6b4bf1a3f8fc662bbaf
Tristan Greffe <tristan.greffe@hotmail.com>
2023-09-25 12:59:18 +0200

    fix: base url #147 [build doc] [skip app]

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  base: '/',
+  base: '/kApp',
   title: 'kApp',
   description: 'kApp - KDK application template',
   ignoreDeadLinks: true,
