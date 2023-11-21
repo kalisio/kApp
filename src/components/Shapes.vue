ad10011346191732a83edcd73d86cc8dddeb0d69
cnouguier <ch.nouguier@gmail.com>
2023-11-21 15:23:12 +0100

    refactor: Shapes.js du to https://github.com/kalisio/kdk/issues/795

--- src/components/Shapes.vue
+++ src/components/Shapes.vue
@@ -6,9 +6,9 @@
       </tr>
     </thead>
     <tbody>
-      <template v-for="shape in shapes" :key="shape.type">
+      <template v-for="shape in shapes" :key="shape.shape">
         <tr>
-          <td>{{ shape.type }}</td>
+          <td>{{ shape.tooltip }}</td>
           <td>
             <KShape v-bind="shape" />
           </td>
@@ -22,13 +22,15 @@
 import { ref } from 'vue'
 
 const shapes = ref([
-  { type: 'circle', tooltip: 'Circle' },
-  { type: 'ellipse', width: 24, tooltip: 'Ellipse' },
-  { type: 'rect', tooltip: 'Rect' },
-  { type: 'triangle-up', tooltip: 'Triangle up' },
-  { type: 'triangle-down', tooltip: 'Triangle down' },
-  { type: 'triangle-right', tooltip: 'Triangle right' },
-  { type: 'triangle-left', tooltip: 'Triangle left' },
-  { type: 'diamond', tooltip: 'Diamond' },
+  { shape: 'circle', tooltip: 'Circle', fill: 'lightgrey', stroke: 'grey', strokeWidth: '1' },
+  { shape: 'circle', width: 24, tooltip: 'Ellipse', fill: 'red', stroke: 'grey', strokeWidth: '2' },
+  { shape: 'rect', tooltip: 'Rect', fill: 'blue', stroke: 'grey', strokeWidth: '3' },
+  { shape: 'triangle', tooltip: 'Triangle up', fill: 'green', stroke: 'grey', strokeWidth: '4' },
+  { shape: 'triangle-down', tooltip: 'Triangle down', fill: 'orange', stroke: 'grey', strokeWidth: '2' },
+  { shape: 'triangle-right', tooltip: 'Triangle right', fill: 'red', stroke: 'grey', strokeWidth: '3' },
+  { shape: 'triangle-left', tooltip: 'Triangle left', fill: 'purple', stroke: 'grey', strokeWidth: '1' },
+  { shape: 'diamond', tooltip: 'Diamond', fill: 'magenta', stroke: 'grey', strokeWidth: '2' },
+  { shape: 'marker-pin', tooltip: 'Marker Pin', fill: 'darkgreen', stroke: 'grey', strokeWidth: '1' },
+  { shape: 'square-pin', tooltip: 'Square Pin', fill: 'turquoise', stroke: 'grey', strokeWidth: '1' },
 ])
 </script>
