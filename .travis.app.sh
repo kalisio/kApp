6e3433e6c0df691b967f4575dfd5912d6ef1c4c9
cnouguier <ch.nouguier@gmail.com>
2023-11-28 00:20:20 +0100

    chore: use development project for building

--- .travis.app.sh
+++ .travis.app.sh
@@ -1,11 +1,90 @@
 #!/bin/bash
 
+check_code()
+{
+   if [[ $1 -ne $2 ]]; then
+	  echo "$3 has failed [error: $1]"
+	  exit 1
+  fi
+}
+
+parse_semver()
+{
+  local REGEXP="^([0-9]+)\.([0-9]+)\.([0-9]+)"
+  [[ "$1" =~ $REGEXP ]]
+  SEMVER=(${BASH_REMATCH[1]} ${BASH_REMATCH[2]} ${BASH_REMATCH[3]})
+}
+
 #
 # Provision the required files
 #
 travis_fold start "provision"
 
-source .travis.env.sh
+# Define the application name
+APP=$(node -p -e "require('./package.json').name")
+
+# Define the application version
+VERSION=$(node -p -e "require('./package.json').version")
+parse_semver $VERSION
+MAJOR=${SEMVER[0]}
+MINOR=${SEMVER[1]}
+PATCH=${SEMVER[2]}
+
+echo "Building $APP v$MAJOR.$MINOR.$PATCH"
+
+# Define the flavor build
+TEST_FLAVOR_REGEX="^test-*|-test$"
+PROD_FLAVOR_REGEX="^prod-v[0-9]+\.[0-9]+\.[0-9]+"
+if [[ $TRAVIS_TAG =~ $PROD_FLAVOR_REGEX ]];
+then
+  export FLAVOR=prod
+  KDK_PROJECT_FILE=$APP-$VERSION
+else
+  if [[ $TRAVIS_BRANCH =~ $TEST_FLAVOR_REGEX ]];
+  then
+    export FLAVOR=test
+    KDK_PROJECT_FILE=$APP-$MAJOR.$MINOR
+  else
+    export FLAVOR=dev
+    KDK_PROJECT_FILE=$APP
+  fi
+fi
+export NODE_APP_INSTANCE=$FLAVOR
+TAG=$VERSION-$FLAVOR
+
+echo "Build flavor is $FLAVOR on branch $TRAVIS_BRANCH"
+
+# Leave the project directory to avoid Webpack to look for files into the project directory
+cd ..
+
+# Clone the workspace where to build the app
+#echo -e "machine github.com\n  login $GITHUB_TOKEN" > ~/.netrc
+git clone https://oauth2:$GITHUB_TOKEN@github.com/kalisio/development.git
+export WORKSPACE_DIR=`pwd`/development/workspaces/apps
+
+# Configue the required env
+source $WORKSPACE_DIR/apps.sh $APP
+
+# setup the BUILD_NUMBER
+export BUILD_NUMBER=$TRAVIS_BUILD_NUMBER
+BUILD_BUCKET=${APP}/$BUILD_NUMBER
+
+# Install the kli
+git clone https://github.com/kalisio/kli.git kalisio && cd kalisio && yarn 
+
+# In dev flavor we can build different versions on different branches
+# so check if a specific file exists for the target branch first otherwise use default one
+if [[ -f $WORKSPACE_DIR/$APP/$FLAVOR/$KDK_PROJECT_FILE-$TRAVIS_BRANCH.js ]];
+then
+  cp $WORKSPACE_DIR/$APP/$FLAVOR/$KDK_PROJECT_FILE-$TRAVIS_BRANCH.js $APP.js
+else
+  cp $WORKSPACE_DIR/$APP/$FLAVOR/$KDK_PROJECT_FILE.js $APP.js
+fi
+
+# Clone the project and install the dependencies
+node . $APP.js --clone
+node . $APP.js --install
+node . $APP.js --link
 
 travis_fold end "provision"
 
