b1131606e732abd93af77067b0fe2d12129c5f09
Robin Bourianes <robin.bourianes@kalisio.com>
2024-02-16 16:53:40 +0100

    wip: build_app workflow

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -7,3 +7,42 @@ THIS_PATH=$(dirname "$THIS_FILE")
 ROOT_PATH=$(dirname "$THIS_PATH")
 
 . "$THIS_PATH/kash/kash.sh"
+
+if [ "$CI" = true ]; then
+    begin_group "Fetching project dependencies ..."
+
+    WORKSPACE_DIR="$(dirname "$ROOT_PATH")"
+    DEVELOPMENT_REPO_DIR="$WORKSPACE_DIR/development"
+
+    # Workaround since repo is kApp with a 'A' and in kli file it's kapp with a 'a'
+    ln -s "$WORKSPACE_DIR/kApp" "$WORKSPACE_DIR/kapp"
+
+    # clone developement
+    git clone --depth 1 "https://$GITHUB_DEVELOPMENT_PAT@github.com/kalisio/development.git" "$DEVELOPMENT_REPO_DIR"
+
+    get_app_infos "$ROOT_PATH" "$DEVELOPMENT_REPO_DIR/workspaces/apps"
+    APP="${APP_INFOS[0]}"
+    VERSION="${APP_INFOS[1]}"
+    FLAVOR="${APP_INFOS[2]}"
+    KLI_FILE="${APP_INFOS[3]}"
+
+    run_kli "$WORKSPACE_DIR" "$KLI_FILE" 16
+
+    end_group "Fetching project dependencies ..."
+
+    echo "Used kli file $KLI_FILE ..."
+    echo "About to build app ${APP}@${VERSION} ($FLAVOR) ..."
+else
+    DEVELOPMENT_REPO_DIR="$KALISIO_DEVELOPMENT_DIR/development"
+fi
+
+## Load project env
+##
+
+. "$DEVELOPMENT_REPO_DIR/workspaces/apps/apps.sh" kapp
+
+## Build app
+##
+
+use_node 16
+yarn pwa:build
