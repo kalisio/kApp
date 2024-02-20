453f63ca89a95dfc512927bdd3229b3eae38e0b5
Robin Bourianes <robin.bourianes@kalisio.com>
2024-02-20 11:31:37 +0100

    wip: run_tests ci script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,10 +3,13 @@ set -euo pipefail
 # set -x
 
 THIS_FILE=$(readlink -f "${BASH_SOURCE[0]}")
-THIS_PATH=$(dirname "$THIS_FILE")
-ROOT_PATH=$(dirname "$THIS_PATH")
+THIS_DIR=$(dirname "$THIS_FILE")
+ROOT_DIR=$(dirname "$THIS_DIR")
 
-. "$THIS_PATH/lib.sh"
+. "$THIS_DIR/lib.sh"
+
+## Parse options
+##
 
 NODE_VER=16
 MONGO_VER=4
@@ -21,33 +24,17 @@ while getopts "m:n:" option; do
     esac
 done
 
-if [ "$CI" = true ]; then
-    begin_group "Fetching project dependencies ..."
+### Init workspace
+###
 
-    WORKSPACE_DIR="$(dirname "$ROOT_PATH")"
-    DEVELOPMENT_REPO_DIR="$WORKSPACE_DIR/development"
+WORKSPACE_DIR="$(dirname "$ROOT_DIR")"
+init_app_infos "$ROOT_DIR" "$WORKSPACE_DIR/development/workspaces/apps"
 
-    # Workaround since repo is kApp with a 'A' and in kli file it's kapp with a 'a'
-    ln -s "$WORKSPACE_DIR/kApp" "$WORKSPACE_DIR/kapp"
+APP=$(get_app_name)
+VERSION=$(get_app_version)
+FLAVOR=$(get_app_flavor)
 
-    # clone developement
-    git clone --depth 1 "https://$GITHUB_DEVELOPMENT_PAT@github.com/kalisio/development.git" "$DEVELOPMENT_REPO_DIR"
-
-    get_app_infos "$ROOT_PATH" "$DEVELOPMENT_REPO_DIR/workspaces/apps"
-    APP="${APP_INFOS[0]}"
-    VERSION="${APP_INFOS[1]}"
-    FLAVOR="${APP_INFOS[2]}"
-    KLI_FILE="${APP_INFOS[3]}"
-
-    run_kli "$WORKSPACE_DIR" "$KLI_FILE" "$NODE_VER"
-
-    end_group "Fetching project dependencies ..."
-
-    echo "Used kli file $KLI_FILE ..."
-    echo "About to run tests for ${APP}@${VERSION} ($FLAVOR) ..."
-else
-    DEVELOPMENT_REPO_DIR="$KALISIO_DEVELOPMENT_DIR/development"
-fi
+echo "About to run tests for ${APP} v${VERSION}-($FLAVOR) ..."
 
 ## Start mongo
 ##
@@ -62,7 +49,7 @@ end_group "Starting mongo $MONGO_VER ..."
 ## Load project env
 ##
 
-. "$DEVELOPMENT_REPO_DIR/workspaces/apps/apps.sh" kapp
+. "$WORKSPACE_DIR/development/workspaces/apps/apps.sh" kapp
 
 ## Run tests
 ##
