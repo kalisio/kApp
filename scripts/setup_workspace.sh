d2760a37c8791705052edbceaa16ea02e90e585f
Robin Bourianes <robin.bourianes@kalisio.com>
2024-03-05 13:15:36 +0100

    chore: cleanup repo

--- scripts/setup_workspace.sh
+++ scripts/setup_workspace.sh
@@ -14,11 +14,12 @@ ROOT_DIR=$(dirname "$THIS_DIR")
 WORKSPACE_BRANCH=
 WORKSPACE_TAG=
 WORKSPACE_NODE=16
-WORKSPACE_FULL=false
-OPT_LIST="n:f"
+WORKSPACE_KIND=klifull
+OPT_LIST="n:k:"
 if [ "$CI" != true ]; then
-    OPT_LIST="b:n:t:f"
+    OPT_LIST="b:n:t:k:"
 fi
+
 while getopts "$OPT_LIST" OPT; do
     case $OPT in
         b) # defines branch
@@ -27,26 +28,29 @@ while getopts "$OPT_LIST" OPT; do
             WORKSPACE_NODE=$OPTARG;;
         t) # defines venv tag
             WORKSPACE_TAG=$OPTARG;;
-        f) # to use kli to install & link = full
-            WORKSPACE_FULL=true;;
+        k) # workspace kind (nokli kli klifull)
+            WORKSPACE_KIND=$OPTARG;;
         *)
         ;;
     esac
 done
 
-shift $((OPTIND-1))
-WORKSPACE_DIR="${1:-$(dirname "$ROOT_DIR")}"
-DEVELOPMENT_DIR="$WORKSPACE_DIR/development"
-
-begin_group "Cloning development repo ..."
+begin_group "Setting up workspace ..."
 
 if [ "$CI" = true ]; then
+    WORKSPACE_DIR="$(dirname "$ROOT_DIR")"
+
     # workaround since repo is kApp with a 'A' and in kli file it's kapp with a 'a'
     mv "$WORKSPACE_DIR/kApp" "$WORKSPACE_DIR/kapp"
     ln -s "$WORKSPACE_DIR/kapp" "$WORKSPACE_DIR/kApp"
 
     DEVELOPMENT_REPO_URL="https://$GITHUB_DEVELOPMENT_PAT@github.com/kalisio/development.git"
 else
+    shift $((OPTIND-1))
+    WORKSPACE_DIR="$1"
+
+    # NOTE: cloning kapp could be avoided if we could parse app_version from tag/branch name instead
+    # In this case, the kli would clone kapp
     GIT_OPS=
     if [ -n "$WORKSPACE_TAG" ] || [ -n "$WORKSPACE_BRANCH" ]; then
         GIT_OPS="--branch ${WORKSPACE_TAG:-$WORKSPACE_BRANCH}"
@@ -60,30 +64,20 @@ else
 fi
 
 # clone development in $WORKSPACE_DIR
+DEVELOPMENT_DIR="$WORKSPACE_DIR/development"
 git clone --depth 1 "$DEVELOPMENT_REPO_URL" "$DEVELOPMENT_DIR"
 
-end_group "Cloning development repo ..."
+if [ "$WORKSPACE_KIND" = kli ] || [ "$WORKSPACE_KIND" = klifull ]; then
+    # select kli file for dependencies
+    init_app_infos "$WORKSPACE_DIR/kapp" "$DEVELOPMENT_DIR/workspaces/apps"
+    KLI_FILE=$(get_app_kli_file)
 
-# select kli file for dependencies
-init_app_infos "$WORKSPACE_DIR/kapp" "$DEVELOPMENT_DIR/workspaces/apps"
-KLI_FILE=$(get_app_kli_file)
-
-echo "About to setup workspace using $KLI_FILE ..."
-
-begin_group "Running kli ..."
-
-# clone kli in venv if not there
-if [ ! -d "$WORKSPACE_DIR/kli" ]; then
-    git clone --depth 1 "https://github.com/kalisio/kli.git" "$WORKSPACE_DIR/kli"
-    cd "$WORKSPACE_DIR/kli" && nvm exec "$WORKSPACE_NODE" yarn install && cd ~-
+    echo "About to populate workspace using $KLI_FILE ..."
+    run_kli "$WORKSPACE_DIR" "$WORKSPACE_NODE" "$KLI_FILE" "$WORKSPACE_KIND"
 fi
 
-cd "$WORKSPACE_DIR"
-nvm exec "$WORKSPACE_NODE" node "$WORKSPACE_DIR/kli/index.js" "$KLI_FILE" --clone --shallow-clone
-if [ "$WORKSPACE_FULL" = true ]; then
-    nvm exec "$WORKSPACE_NODE" node "$WORKSPACE_DIR/kli/index.js" "$KLI_FILE" --install
-    nvm exec "$WORKSPACE_NODE" node "$WORKSPACE_DIR/kli/index.js" "$KLI_FILE" --link --link-folder "$WORKSPACE_DIR/yarn-links"
-fi
-cd ~-
+end_group "Setting up workspace ..."
 
-end_group "Running kli ..."
+if [ -n "${KLI_FILE:-}" ]; then
+    echo "Workspace setup using $KLI_FILE"
+fi
