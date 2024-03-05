ffa1677b2a3399cd0d3574088347cabaad5df4e1
Robin Bourianes <robin.bourianes@kalisio.com>
2024-03-05 15:36:15 +0100

    fix: fixing build docs workflow

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -37,11 +37,13 @@ rm -f .postcssrc.js && cd docs && yarn install && yarn build
 if [ "$PUBLISH" = true ]; then
     load_env_files "$WORKSPACE_DIR/development/common/KAPP_PUSH_DOC_TOKEN.enc.env"
 
-    GIT_COMMIT_SHA=$(get_git_commit_sha "$ROOT_DIR")
-    GIT_COMMITTER=$(get_git_committer_infos "$ROOT_DIR")
+    COMMIT_SHA=$(get_git_commit_sha "$ROOT_DIR")
+    COMMIT_AUTHOR_NAME=$(get_git_commit_author_name "$ROOT_DIR")
+    COMMIT_AUTHOR_EMAIL=$(get_git_commit_author_email "$ROOT_DIR")
     deploy_gh_pages \
         "https://oauth2:$KAPP_PUSH_DOC_TOKEN@github.com/kalisio/kApp.git" \
         "$ROOT_DIR/docs/.vitepress/dist" \
-        "$GIT_COMMITTER" \
-        "Docs built from $GIT_COMMIT_SHA"
+        "$COMMIT_AUTHOR_NAME" \
+        "$COMMIT_AUTHOR_EMAIL" \
+        "Docs built from $COMMIT_SHA"
 fi
