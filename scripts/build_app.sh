cd2d9f5f94e2e6fe135a5c61d06bc164bdcc1b9b
Robin Bourianes <robin.bourianes@kalisio.com>
2024-03-13 08:55:54 +0100

    refactor: use slack_ci_report from kash

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -8,68 +8,7 @@ ROOT_DIR=$(dirname "$THIS_DIR")
 
 . "$THIS_DIR/kash/kash.sh"
 
-init_repo_infos() {
-    local REPO_DIR="$1"
-    local REPO_NAME="$(basename "$REPO_DIR")"
-    cd "$REPO_DIR"
-    local REPO_TAG=$(git tag --points-at)
-    local REPO_BRANCH=$(git branch --show-current)
-    local COMMIT_SHA=$(git rev-parse HEAD)
-    local COMMIT_AUTHOR=$(git show -s --pretty=%an)
-    local COMMIT_EMAIL=$(git show -s --pretty=%ae)
-    local COMMIT_MESSAGE=$(git show -s --pretty=%B)
-    cd ~-
-
-    REPO_INFOS=("$REPO_NAME" "$REPO_TAG" "$REPO_BRANCH" "$COMMIT_SHA" "$COMMIT_AUTHOR" "$COMMIT_EMAIL" "$COMMIT_MESSAGE")
-}
-
-init_ci_infos() {
-    local JOB_URL
-    local REPO_URL
-    case "$CI_ID" in
-        github)
-            REPO_URL="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/commit/"
-            JOB_NAME="$GITHUB_WORKFLOW"
-            JOB_URL="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
-            ;;
-        travis)
-            REPO_URL="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/commit/"
-            JOB_NAME="$TRAVIS_JOB_NAME"
-            JOB_URL="$TRAVIS_JOB_WEB_URL"
-            ;;
-        gitlab)
-            REPO_URL="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/commit/"
-            JOB_NAME="$FOO"
-            JOB_URL="$TRAVIS_JOB_WEB_URL"
-            ;;
-        *)
-            REPO_URL="http://github.com/kalisio/kapp/commit/"
-            JOB_NAME="Build app"
-            JOB_URL="http://github.com/kalisio/kapp/actions/runs/8249597899"
-            ;;
-    esac
-
-    CI_INFOS=("$REPO_URL" "$JOB_NAME" "$JOB_URL")
-}
-
-slack_ci_report() {
-    local REPO_DIR="$1"
-    local RET_CODE="$2"
-    local SLACK_WEBHOOK="$3"
-    local STATUS="success"
-    local COLOR="#2eb886"
-    if [ "$RET_CODE" != "0" ]; then STATUS="failed"; COLOR="#a30200"; fi
-    local MESSAGE
-
-    init_repo_infos "$REPO_DIR"
-    init_ci_infos
-
-    local COMMIT_URL="${CI_INFOS[0]}/${REPO_INFOS[3]}"
-    local REPO_REF="${REPO_INFOS[1]}"
-    if [ "$REPO_REF" = "" ]; then REPO_REF="${REPO_INFOS[2]}"; fi
-    MESSAGE=$(printf "[%s@%s] <%s|%s> %s (%s, <%s|%s>)" "${REPO_INFOS[0]}" "$REPO_REF" "${CI_INFOS[2]}" "${CI_INFOS[1]}" "$STATUS" "${REPO_INFOS[4]}" "$COMMIT_URL" "${REPO_INFOS[3]}")
-    slack_color_log "$SLACK_WEBHOOK" "$MESSAGE" "$COLOR"
-}
+trap 'slack_ci_report "$ROOT_DIR" "$?" "$SLACK_WEBHOOK_APPS"' EXIT
 
 ## Parse options
 ##
