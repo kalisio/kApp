fc63a0ea9b6fb07507532fa58ea4b0064c0d606c
cnouguier <ch.nouguier@gmail.com>
2024-04-05 18:57:29 +0200

    chore: replaced KTerms by KDocument

--- config/default.js
+++ config/default.js
@@ -199,7 +199,8 @@ module.exports = {
       id: 'terms-policies', 
       label: 'screen.TERMS_AND_POLICIES', 
       dialog: {
-        component: 'app/KTerms'
+        component: 'document/KDocument',
+        url: 'kapp-terms.md'
       }
     }],
     // frameBackgroundColor: '#FFDC9E',
