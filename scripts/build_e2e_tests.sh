ba421f91613a21623ea3eadfe9a2a0938ad1a770
tristan-greffe <tristan.greffe@hotmail.com>
2024-04-08 11:01:23 +0200

    chore: improve e2e tests

--- scripts/build_e2e_tests.sh
+++ scripts/build_e2e_tests.sh
@@ -34,7 +34,7 @@ FLAVOR=$(get_app_flavor)
 
 echo "About to build ${APP} v${VERSION}-$FLAVOR ..."
 
-load_env_files "$WORKSPACE_DIR/development/common/kalisio_harbor.enc.env" "$WORKSPACE_DIR/development/common/SLACK_WEBHOOK_APPS.enc.env"
+load_env_files "$WORKSPACE_DIR/development/common/kalisio_harbor.enc.env"
 load_value_files "$WORKSPACE_DIR/development/common/KALISIO_HARBOR_PASSWORD.enc.value"
 
 ## Build container
@@ -52,7 +52,6 @@ DOCKER_BUILDKIT=1 docker build \
     --build-arg NODE_APP_INSTANCE="$FLAVOR" \
     --build-arg SUBDOMAIN="$FLAVOR.kalisio.xyz" \
     --build-arg HEADLESS=true \
-    --build-arg SLACK_WEBHOOK_APPS="$SLACK_WEBHOOK_APPS" \
     -f e2e-tests.Dockerfile \
     -t "$IMAGE_NAME:$IMAGE_TAG" \
     "$WORKSPACE_DIR"
