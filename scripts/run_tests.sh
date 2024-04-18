39e603981cee99a3efc5679ef16e4b703a89fa42
Robin Bourianes <robin.bourianes@kalisio.com>
2024-04-18 13:00:16 +0200

    wip: use run_app_tests from kash [additional tests]

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -37,48 +37,6 @@ while getopts "m:n:cr:" option; do
     esac
 done
 
-run_app_tests() {
-    local REPO_DIR="$1"
-    local KLI_BASE="$2"
-    local CODE_COVERAGE="$3"
-    local NODE_VER="$4"
-    local MONGO_VER="$5"
-    local WORKSPACE_DIR
-    WORKSPACE_DIR="$(dirname "$REPO_DIR")"
-
-    init_app_infos "$REPO_DIR" "$WORKSPACE_DIR/development/$KLI_BASE"
-
-    APP=$(get_app_name)
-    VERSION=$(get_app_version)
-    FLAVOR=$(get_app_flavor)
-
-    echo "About to run tests for $APP v$VERSION-$FLAVOR ..."
-
-    ## Start mongo
-    ##
-
-    if [ -n "$MONGO_VER" ]; then
-        begin_group "Starting mongo $MONGO_VER ..."
-
-        use_mongo "$MONGO_VER"
-        k-mongo
-
-        end_group "Starting mongo $MONGO_VER ..."
-    fi
-
-    ## Run tests
-    ##
-
-    use_node "$NODE_VER"
-    yarn test:server
-
-    ## Publish code coverage
-    ##
-
-    if [ "$CODE_COVERAGE" = true ]; then
-        send_coverage_to_cc "$CC_TEST_REPORTER_ID"
-    fi
-}
 
 ## Init workspace
 ##
