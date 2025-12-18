36ee8b1167c4c974b1eb04b53a47f9a371c58d31
tristan-greffe <tristan.greffe@hotmail.com>
2025-12-18 16:00:09 +0100

    fix: make lint happy

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -1,3 +1,4 @@
+/* eslint-env serviceworker */
 import { precacheAndRoute } from 'workbox-precaching'
 import { registerRoute } from 'workbox-routing'
 import { NetworkFirst } from 'workbox-strategies'
